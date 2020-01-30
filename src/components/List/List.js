5f8321c01cc6886ae3f4e872b48dfa8845cd55ab
Jarosław Szewczyk <jaroslaw.szewczyk@tutanota.com>
2020-01-30 21:21:03 +0000

    Add type check, and Column component

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero';
+import PropTypes from 'prop-types';
+import Column from '../Column/Column';
 
 class List extends React.Component {
+  static propTypes = {
+    title: PropTypes.node.isRequired,
+    children: PropTypes.node,
+    text: PropTypes.string.isRequired,
+  }
+
+  static defaultProps = {
+    children: <p>I can do all the things!!!</p>
+  }
   render() {
     return (
       <section className={styles.component}>
-        <Hero />
+        <Hero titleText={this.props.title} text={this.props.text}/>
+        <div className={styles.description}>
+          {this.props.children}
+        </div>
+        <div className={styles.columns}>
+          <Column title='Animals' />
+          <Column title='Plants' />
+          <Column title='Minerals'/>
+        </div>
       </section>
     )
   }
