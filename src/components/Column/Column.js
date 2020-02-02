4441242a6c175736f934c4fe3b5d7f121e1e5a7e
Jarosław Szewczyk <jaroslaw.szewczyk@tutanota.com>
2020-02-02 18:47:56 +0000

    Add card component

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
+import Card from '../Card/Card';
 
-const Column = props => (
-    <section className={styles.component}>
-        <h3 className={styles.title}>{props.title}</h3>
-    </section>
-);
+class Column extends React.Component {
+    state = {
+        cards: this.props.cards || [],
+    }
 
-Column.protoTypes = {
-    title: PropTypes.node.isRequired,
+    static protoTypes = {
+        title: PropTypes.string,
+    }
+    render() {
+        // console.log('cards',this.state.cards);
+        // console.log(this.state.cards.map(card => {
+        //     console.log('card.key', card.key);
+        // }));
+        return (
+            <section className={styles.component}>
+                <h3 className={styles.title}>{this.props.title}</h3>
+                {this.state.cards.map(({key, ...cardProps}) => (
+                    <Card key={key} {...cardProps} />
+                ))}
+            </section>
+        )
+    }
 }
 
 export default Column;
